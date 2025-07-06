8df606002f927639ee2cd0990e5acdc82701c22f
sdtrd011 <sdtrd011@gmail.com>
2025-07-06 18:16:22 +0900

    Implement Java sort

--- src/main/java/com/fastcampus/javaoop/Logic/JavaSort.java
+++ src/main/java/com/fastcampus/javaoop/Logic/JavaSort.java
@@ -1,2 +1,16 @@
-package com.fastcampus.javaoop.Logic;public class JavaSort {
+package com.fastcampus.javaoop.Logic;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class JavaSort <T extends Comparable<T>>{
+
+    public List<T> sort(List<T> list) {
+        List<T> output = new ArrayList<>(list);
+        Collections.sort(output);
+
+        return output;
+    }
 }
