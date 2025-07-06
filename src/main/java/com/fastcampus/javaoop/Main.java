7856ca826f3fafd73220af91cb6ec344aaa08715
sdtrd011 <sdtrd011@gmail.com>
2025-07-06 21:34:44 +0900

    Update #3 - Main

--- src/main/java/com/fastcampus/javaoop/Main.java
+++ src/main/java/com/fastcampus/javaoop/Main.java
@@ -1,12 +1,14 @@
 package com.fastcampus.javaoop;
 
 import com.fastcampus.javaoop.Logic.BubbleSort;
+import com.fastcampus.javaoop.Logic.JavaSort;
+import com.fastcampus.javaoop.Logic.Sort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> sort = new BubbleSort<>();
+        Sort<String> sort = new JavaSort<>();
 
         System.out.println("[result]" + sort.sort(Arrays.asList(args)));
     }
