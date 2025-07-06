e90a39d6a2e7d9966e705223e8f5c537991ef552
sdtrd011 <sdtrd011@gmail.com>
2025-07-06 18:17:26 +0900

    Implement Main

--- src/main/java/com/fastcampus/javaoop/Main.java
+++ src/main/java/com/fastcampus/javaoop/Main.java
@@ -1,4 +1,13 @@
-package com.fastcampus.java;
+package com.fastcampus.javaoop;
+
+import com.fastcampus.javaoop.Logic.BubbleSort;
+
+import java.util.Arrays;
 
 public class Main {
+    public static void main(String[] args) {
+        BubbleSort<String> sort = new BubbleSort<>();
+
+        System.out.println("[result]" + sort.sort(Arrays.asList(args)));
+    }
 }
